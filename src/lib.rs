1e02ab59b058ac99e8a79e584e0f9edac9871d01
kcw <dwong1375@gmail.com>
2022-07-20 01:21:02 -0700

    implement thread termination via new Message enum, which either sends NewJob or Terminate

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,19 @@
 use std::thread;
 use std::sync::{mpsc, Arc, Mutex};
 
-pub struct ThreadPool {
-    workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
-}
-
 // a Job is an arbitrary function to perform
 type Job = Box<dyn FnOnce() + Send + 'static>;
+
+enum Message {
+    NewJob(Job), // holds a job for thread to execute
+    Terminate, // signals thread to stop
+}
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Message>,
+}
+
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -38,14 +44,26 @@ impl ThreadPool {
     {
         let job = Box::new(f);
 
-        self.sender.send(job).unwrap();
+        self.sender.send(Message::NewJob(job)).unwrap();
     }
 }
 
 impl Drop for ThreadPool {
     fn drop(&mut self) { 
         // clean up the threads and finish their work for graceful shutdown
+        println!("Sending terminate message to all workers.");
 
+        // we cannot guarantee that two send calls in one loop get received by the same thread
+        // thus we need two loops to prevent deadlock
+
+        // first need to loop to terminate all workers
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("Shutting down all workers.");
+
+        // then need to loop to perform final work
         for worker in &mut self.workers {
             println!("Shtting down worker {}", worker.id);
 
@@ -63,19 +81,29 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
         // make the thread keep waiting for a job and then run it
         let thread = thread::spawn(move || loop {
             // the mutex lock ensures that only one worker thread at a time tries to request a job
             // mutex lock is released at the end of the let statement, 
             // allowing next thread to request job while this job executes
-            let job = receiver.lock().unwrap().recv().unwrap();
+            let message = receiver.lock().unwrap().recv().unwrap();
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {id} got a job, executing.");
+                    job();
+                },
+                Message::Terminate => {
+                    println!("Worker {id} told to terminate.");
+                    break;
+                }
+            }
             
-            println!("Worker {id} got a job, executing.");
-
-            job();
         });
 
-        Worker {id, thread: Some(thread)}
+        Worker {
+            id, 
+            thread: Some(thread)
+        }
     }
 }
