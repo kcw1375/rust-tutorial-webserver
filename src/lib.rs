dd2b81ed67a9762b378a0378018284b88a160e76
kcw <dwong1375@gmail.com>
2022-07-20 00:54:30 -0700

    implement job execution; each thread gets a job one at a time for multi-threaded execution

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,8 @@ pub struct ThreadPool {
     sender: mpsc::Sender<Job>,
 }
 
-struct Job;
+// a Job is an arbitrary function to perform
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -20,6 +21,8 @@ impl ThreadPool {
 
         let mut workers = Vec::with_capacity(size);
         // ThreadPool will hold onto the sending side of the channel
+        // receiver must be made into an Arc<Mutex> so that all threads can use one receiver
+        // (only can have a single consumer)
         let (sender, receiver) = mpsc::channel();
         let receiver = Arc::new(Mutex::new(receiver));
         for id in 0..size {
@@ -33,7 +36,9 @@ impl ThreadPool {
     where
         F: FnOnce() + Send + 'static,
     {
-        
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
     }
 }
 
@@ -44,8 +49,16 @@ struct Worker {
 
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(|| {
-            receiver;
+        // make the thread keep waiting for a job and then run it
+        let thread = thread::spawn(move || loop {
+            // the mutex lock ensures that only one worker thread at a time tries to request a job
+            // mutex lock is released at the end of the let statement, 
+            // allowing next thread to request job while this job executes
+            let job = receiver.lock().unwrap().recv().unwrap();
+            
+            println!("Worker {id} got a job, executing.");
+
+            job();
         });
 
         Worker {id, thread}
